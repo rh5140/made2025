ab7a3391681b3ee0aea40be67344359f9e9b0275
Daniel Liu <danielliu@berkeley.edu>
2025-07-13 14:36:44 -0700

    added state machine

--- madegj/Assets/Scripts/Protag/ProtagCore.cs
+++ madegj/Assets/Scripts/Protag/ProtagCore.cs
@@ -1,13 +1,24 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class ProtagCore : MonoBehaviour
 {
 
     enum PlayerState { MOVE, ROLL, AIM, SHOOT, DEAD }
+    public int playerID; // 1 or 2
+
+    public float rollCooldown = 1.0f;
+    public float rollDuration = 2.0f;
+
+    private List<KeyCode> aimKeys = new List<KeyCode> { KeyCode.BackQuote, KeyCode.Period };
+    private List<KeyCode> rollKeys = new List<KeyCode> { KeyCode.Alpha1, KeyCode.Slash };
+
+
+    private float rollPrevTime;
+    private bool hasProjectile;
 
     [SerializeField]
     public ProtagMovement protagMovement;
-
     private PlayerState playerState;
     public Rigidbody2D playerRigidBody2d;
 
@@ -15,16 +26,68 @@ public class ProtagCore : MonoBehaviour
     void Start()
     {
         playerState = PlayerState.MOVE;
+        hasProjectile = true;
+        rollPrevTime = -1 * rollCooldown; // Allow players to roll immediately?
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (playerState == PlayerState.MOVE) protagMovement.HandleMovement();
+        UpdateState();
+        HandleState();
+
     }
 
-    Vector2 GetPosition()
+    private void HandleState()
+    {
+        if (playerState == PlayerState.MOVE) protagMovement.HandleMovement(playerID);
+    }
+
+    private void UpdateState()
+    {
+        // Recover from Roll
+        if (playerState == PlayerState.ROLL && (rollPrevTime <= Time.time - rollDuration))
+        {
+            playerState = PlayerState.MOVE;
+            return;
+        }
+
+        // Move to Roll
+        if (playerState == PlayerState.MOVE && (rollPrevTime <= Time.time - rollCooldown) && Input.GetKeyDown(rollKeys[playerID - 1]))
+        {
+            rollPrevTime = Time.time;
+            playerState = PlayerState.ROLL;
+            return;
+        }
+
+        // Move to Aim
+        if (playerState == PlayerState.MOVE && Input.GetKeyDown(aimKeys[playerID - 1]))
+        {
+            playerState = PlayerState.AIM;
+            return;
+        }
+
+        // Aim to Move
+        if (playerState == PlayerState.AIM && Input.GetKeyUp(aimKeys[playerID - 1]))
+        {
+            playerState = PlayerState.MOVE;
+            return;
+        }
+    }
+
+    void OnGUI()
+    {
+        GUI.Label(new Rect(0, 0, 50, 50), playerState.ToString());
+        GUI.Label(new Rect(0, 50, 50, 50), (rollCooldown - (Time.time - rollPrevTime)).ToString() + " cooldown");
+    }
+
+    public Vector2 GetPosition()
     {
         return playerRigidBody2d.position;
+    }
+
+    public void PickupProjectile()
+    {
+        hasProjectile = true;
     }
 }
--- madegj/Assets/Scripts/Protag/ProtagMovement.cs
+++ madegj/Assets/Scripts/Protag/ProtagMovement.cs
@@ -1,22 +1,28 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class ProtagMovement : MonoBehaviour
 {
 
+    private List<KeyCode> leftKeys = new List<KeyCode> { KeyCode.A, KeyCode.LeftArrow };
+    private List<KeyCode> rightKeys = new List<KeyCode> { KeyCode.D, KeyCode.RightArrow};
+    private List<KeyCode> upKeys = new List<KeyCode> { KeyCode.W, KeyCode.UpArrow};
+    private List<KeyCode> downKeys = new List<KeyCode> { KeyCode.S, KeyCode.DownArrow};
+
     public float movementMultiplier = 5.0f;
 
     public Rigidbody2D playerRigidBody2D;
 
-    public void HandleMovement()
+    public void HandleMovement(int playerID)
     {
         Vector2 newVelocity = Vector2.zero;
-        if (Input.GetKey(KeyCode.A))
+        if (Input.GetKey(leftKeys[playerID - 1]))
             newVelocity += Vector2.left;
-        if (Input.GetKey(KeyCode.D))
+        if (Input.GetKey(rightKeys[playerID - 1]))
             newVelocity += Vector2.right;
-        if (Input.GetKey(KeyCode.W))
+        if (Input.GetKey(upKeys[playerID - 1]))
             newVelocity += Vector2.up;
-        if (Input.GetKey(KeyCode.S))
+        if (Input.GetKey(downKeys[playerID - 1]))
             newVelocity += Vector2.down;
 
         newVelocity = newVelocity.normalized;
