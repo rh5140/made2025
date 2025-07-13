03db20aa94f15281516553e3483bd5c55dd9b398
Daniel Liu <danielliu@berkeley.edu>
2025-07-13 15:24:05 -0700

    add roll

--- madegj/Assets/Scripts/Protag/ProtagCore.cs
+++ madegj/Assets/Scripts/Protag/ProtagCore.cs
@@ -1,30 +1,33 @@
 using System.Collections.Generic;
+using System.Numerics;
 using UnityEngine;
+using Vector2 = UnityEngine.Vector2;
 
 public class ProtagCore : MonoBehaviour
 {
 
-    enum PlayerState { MOVE, ROLL, AIM, SHOOT, DEAD }
+    public enum PlayerState { MOVE, ROLL, AIM, SHOOT, DEAD }
     public int playerID; // 1 or 2
 
     public float rollCooldown = 1.0f;
     public float rollDuration = 2.0f;
+    public float rollMovementMultiplier = 5.0f;
+    public Vector2 prevDirection = Vector2.up;
 
-    private List<KeyCode> aimKeys = new List<KeyCode> { KeyCode.BackQuote, KeyCode.Period };
-    private List<KeyCode> rollKeys = new List<KeyCode> { KeyCode.Alpha1, KeyCode.Slash };
+    public List<KeyCode> aimKeys = new List<KeyCode> { KeyCode.BackQuote, KeyCode.Period };
+    public List<KeyCode> rollKeys = new List<KeyCode> { KeyCode.Alpha1, KeyCode.Slash };
     public List<KeyCode> leftKeys = new List<KeyCode> { KeyCode.A, KeyCode.LeftArrow };
     public List<KeyCode> rightKeys = new List<KeyCode> { KeyCode.D, KeyCode.RightArrow };
     public List<KeyCode> upKeys = new List<KeyCode> { KeyCode.W, KeyCode.UpArrow };
     public List<KeyCode> downKeys = new List<KeyCode> { KeyCode.S, KeyCode.DownArrow };
     public float movementMultiplier = 5.0f;
 
-
     private float rollPrevTime;
     private bool hasProjectile;
 
     [SerializeField]
     public ProtagMovement protagMovement;
-    private PlayerState playerState;
+    public PlayerState playerState;
     public Rigidbody2D playerRigidBody2d;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -45,7 +48,15 @@ public class ProtagCore : MonoBehaviour
 
     private void HandleState()
     {
-        if (playerState == PlayerState.MOVE) protagMovement.HandleMovement(playerID);
+        if (playerState == PlayerState.MOVE) {
+            protagMovement.HandleMovement();
+            return;
+        }
+        if (playerState == PlayerState.ROLL)
+        {
+            protagMovement.HandleRoll();
+            return;
+        }
     }
 
     private void UpdateState()
--- madegj/Assets/Scripts/Protag/ProtagMovement.cs
+++ madegj/Assets/Scripts/Protag/ProtagMovement.cs
@@ -9,9 +9,11 @@ public class ProtagMovement : MonoBehaviour
 
     public Rigidbody2D playerRigidBody2D;
 
-    public void HandleMovement(int playerID)
+    public void HandleMovement()
     {
+        int playerID = protagCore.playerID;
         Vector2 newVelocity = Vector2.zero;
+
         if (Input.GetKey(protagCore.leftKeys[playerID - 1]))
             newVelocity += Vector2.left;
         if (Input.GetKey(protagCore.rightKeys[playerID - 1]))
@@ -21,8 +23,16 @@ public class ProtagMovement : MonoBehaviour
         if (Input.GetKey(protagCore.downKeys[playerID - 1]))
             newVelocity += Vector2.down;
 
-        newVelocity = newVelocity.normalized;
+        Vector2 direction = newVelocity.normalized;
+        if (direction != Vector2.zero)
+            protagCore.prevDirection = direction;
 
-        playerRigidBody2D.linearVelocity = newVelocity * protagCore.movementMultiplier;
+        playerRigidBody2D.linearVelocity = direction * protagCore.movementMultiplier;
+    }
+
+    public void HandleRoll() {
+        Debug.Log("handling roll...");
+        Debug.Log(protagCore.prevDirection);
+        playerRigidBody2D.linearVelocity = protagCore.prevDirection * protagCore.rollMovementMultiplier;
     }
 }
